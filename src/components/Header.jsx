d925405c9ac16cbb3d861d9768e054dcfcd4d0e3
DavidR95STW <juandasv3@gmail.com>
2025-10-31 12:53:55 -0500

    Agregado de Actualizacion home y header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,14 +4,13 @@ const Header = () => {
   return (
     <header className="header">
       <div className="header-container">
-        <h1 className="header-title">Mi Sitio Web</h1>
+        <h1 className="header-title">Mi Aplicacion</h1>
         <nav className="header-nav">
           <ul className="nav-list">
-            <li><a href="/">Inicio</a></li>
-            <li><a href="/login">Login</a></li>
-            <li><a href="/Register">Registrarse</a></li>
-            <li><a href="/Register">Contacto</a></li>
-            <li><a href="/homero">Homero</a></li>
+             <li><a className="enlace-header" href="">Inicio de sesion</a></li>
+            <li><a className="boton-registro" href="/register">Registrarse</a></li>
+            <li><a className="enlace-header" href="/login">Login</a></li>
+            <li><a className="enlace-header" href="/homero">Homero</a></li>
           </ul>
         </nav>
       </div>
